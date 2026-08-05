f205a32f2f889474fbb62d9dca2c07d93cf24751
Sebastian Rucabado <sebastianrucabado123@gmail.com>
2026-08-05 15:55:24 +0200

    Added relative scaling

--- src/global.ts
+++ src/global.ts
@@ -4,3 +4,4 @@ import type { PlayerState } from './state.ts';
 export const speed = 7;
 export const color = `rgb(${TSCMath.pickRandom(0, 175)}, ${TSCMath.pickRandom(0, 175)}, ${TSCMath.pickRandom(0, 175)})`;
 export const conn = new Multiplayer<PlayerState>('localhost:3000');
+export const baseRadius = 130;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { Circle, Engine, Pen, Text } from 'tscratch';
+import { Circle, Engine, Pen, Text, Watermark } from 'tscratch';
 import './events/controls.ts';
 import startGame from './logic/start-game.ts';
-import { pen } from './sprites/main.ts';
-import { conn } from './global.ts';
+import { pen, text } from './sprites/main.ts';
+import { conn, baseRadius } from './global.ts';
 
 const engine = Engine.init();
 engine.setScene('lobby');
@@ -17,16 +17,29 @@ engine.setLoop('game', () => {
 
     // Draw each sprite (relative to the player)
     const playerState = conn.getRoomPlayerState();
+    const coeff = baseRadius / playerState.radius; // For relative scaling
+
     for (const state of conn.roomClients.values()) {
-        if (!state.alive) continue;
+        if (!state.alive || JSON.stringify(playerState) === JSON.stringify(state)) continue;
         
         Pen.drawSprite(Circle, {
-            x: state.x - playerState.x,
-            y: state.y - playerState.y,
-            radius: state.radius,
+            x: coeff * (state.x - playerState.x), // Relative positioning
+            y: coeff * (state.y - playerState.y),
+            radius: coeff * state.radius,
             color: state.color
         });
     }
+
+    // Draw
+    if (playerState.alive) {
+        Pen.drawSprite(Circle, {
+            radius: baseRadius,
+            color: playerState.color
+        });
+    }
+
+    // Render the kill count
+    text.setContent(`Kills: ${playerState.kills}`);
 
     // Show a death message if eliminated
     if (!playerState.alive) {
